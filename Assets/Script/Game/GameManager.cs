afa93f27643ca397a3f643815bd181414e7965da
pure <1035617247@qq.com>
2024-04-28 23:01:52 +0800

    小幅度更新

---
+++ Assets/Script/Game/GameManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GoundBackItem[,] GoundBackItemArray2D;
+
+	void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
