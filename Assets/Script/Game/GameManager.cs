0c14f0736228fd3abe8fcb4b634fd70ab33ac822
1035617247 <1035617247@qq.com>
2024-04-29 18:40:37 +0800

    同步最新11

--- Assets/Script/Game/GameManager.cs
+++ Assets/Script/Game/GameManager.cs
@@ -1,10 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[System.Serializable]
 public class GameManager : MonoBehaviour
 {
     public GoundBackItem[,] GoundBackItemArray2D;
+
+
+    public void SetGoundBack(int x,int y){
+        GoundBackItemArray2D= new GoundBackItem[x, y];
+	}
+
+    public void EnterSurface(int x,int y)
+    {
+
+    }
 
 	void Start()
     {
