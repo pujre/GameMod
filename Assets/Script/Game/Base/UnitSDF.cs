6b373a3679398b222b6ee781bddf5d246b9017cd
pure <1035617247@qq.com>
2025-01-16 11:29:32 +0800

    1：移除 一些无用代码

--- Assets/Script/Game/Base/UnitSDF.cs
+++ Assets/Script/Game/Base/UnitSDF.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using WeChatWASM;
 
 
 public class UnitSDF : MonoBehaviour
@@ -121,102 +120,6 @@ public class UnitSDF : MonoBehaviour
 		
 	}
 
-	#region Old script
-	/// <summary>
-	/// 深度搜索
-	/// </summary>
-	/// <param name="coordinates"></param>
-	/// <returns></returns>
-	//public static List<InstructionData> FilterLinkedCoordinates(List<Vector2Int> coordinates)
-	//{
-	//	List<InstructionData> instructionData = new List<InstructionData>();
-	//	Debug.Log("____________________________________");
-	//	// 查找起点
-	//	Vector2Int start = FindCountIsOne(coordinates);
-	//	Debug.Log(string.Format("查找起点-筛选后点的坐标为X:{0},Y:{1},他的周围点为{2}", start.x, start.y, GameManager.Instance.GetAroundCanBeOperatedPos(start.x, start.y).Count.ToString()));
-
-	//	// 用于记录访问过的节点和未访问的邻居
-	//	Dictionary<Vector2Int, InstDataCalculus> visitedNodes = new Dictionary<Vector2Int, InstDataCalculus>();
-	//	bool isOn=false;
-	//	int index = 0;
-	//	HashSet<Vector2Int> visited = new HashSet<Vector2Int>();
-	//	while(!isOn)
-	//	{
-	//		List<Vector2Int> around = GameManager.Instance.GetAroundPos(start.x, start.y).Where(pos => coordinates.Contains(pos)&& !visited.Contains(pos)).ToList(); // 获取当前坐标周围的坐标
-	//		string aroundString = string.Join(", ", around.Select(pos => pos.ToString()).ToArray());
-	//		Debug.Log(string.Format("坐标为：{0},长度为：{1},Index为：{2},已访问过的数为：{3}，当前总数为：{4}，含有以下这些点：{5}", start,around.Count,index,visited.Count, coordinates.Count, aroundString));
-	//		if (around.Count == 0)
-	//		{
-	//			if (instructionData.Count == coordinates.Count - 1)
-	//			{
-	//				Debug.Log("没有更多节点可以访问，结束,返回的路径长度为："+ instructionData.Count);
-	//				isOn = true;
-	//				break;
-	//			}
-
-	//			// 回溯：当前节点没有未访问的邻居，从记录中取回上一个节点
-	//			var lastEntry = visitedNodes.Last();
-	//			start = lastEntry.Key;
-	//			around = lastEntry.Value.VisitedNodes.Except(new List<Vector2Int> { start }).ToList();
-	//			int backtrackCount = index - lastEntry.Value.Index;
-	//               for (int i = 0; i < backtrackCount; i++)
-	//			{
-	//				visited.Remove(instructionData[instructionData.Count - 1].StarVector2);
-	//			}
-	//               instructionData.RemoveRange(instructionData.Count - backtrackCount, backtrackCount);
-	//			index -= backtrackCount;
-
-	//			Debug.Log("回溯了" + backtrackCount + "次");
-	//			//continue;
-	//		}
-
-
-	//		if (around.Count == 1)
-	//		{
-	//			Vector2Int next = around[0];
-	//			instructionData.Add(new InstructionData(start, next));
-	//			visited.Add(start);
-	//			Debug.Log(string.Format("添加了一个InstructionData，start为：{0},end为：{1},", start, next));
-	//			start = next;
-	//			index++;
-	//			if (visitedNodes.ContainsKey(start)) {
-	//				visitedNodes.Remove(start);
-	//			}
-
-	//		}
-	//		else if (around.Count > 1)
-	//		{
-	//			if (GameManager.Instance.GetAroundPos(around[0].x, around[0].y).Where(pos => coordinates.Contains(pos)).ToList().Count==1) {
-	//				index++;
-	//				UpdateVisitedNodes(visitedNodes, start, index, around);
-	//				instructionData.Add(new InstructionData(around[0], start));
-	//				Debug.Log(string.Format("添加了一个InstructionData，start为：{0},end为：{1},", around[0], start));
-	//				visited.Add(around[0]);
-	//			}
-	//			else {
-	//				index++;
-	//				// 记录当前节点及其未访问的邻居
-	//				UpdateVisitedNodes(visitedNodes, start, index, around);
-	//				Vector2Int next = around[0];
-	//				instructionData.Add(new InstructionData(start, next));
-	//				Debug.Log(string.Format("添加了一个InstructionData，start为：{0},end为：{1},", start, next));
-	//				visited.Add(start);
-	//				start = next;
-	//			}
-	//		}
-	//		else
-	//		{
-	//			// 没有更多节点可以访问，结束
-	//			Debug.Log("没有更多节点可以访问，结束,返回的路径长度为：" + instructionData.Count);
-	//			isOn = true;
-	//			break;
-	//		}
-	//	}
-
-	//	return instructionData;
-	//}
-	#endregion
-
 
 	public static List<InstructionData> FilterLinkedCoordinates(Vector2Int currentPoint, List<Vector2Int> surroundingPoints)
 	{
@@ -260,70 +163,7 @@ public class UnitSDF : MonoBehaviour
 		return instructions;
 	}
 
-	private float GetEuclideanDistance(Vector2Int a, Vector2Int b)
-	{
-		return Vector2Int.Distance(a, b);
-	}
-
-
-	#region Test 1
-	public static List<InstructionData> FilterLinkedCoordinates(List<Vector2Int> coordinates)
-	{
-		List<InstructionData> instructionData = new List<InstructionData>();
-		LogManager.Instance.Log("___________________________深度搜索_____________________________");
-		// 使用HashSet提高查找效率
-		HashSet<Vector2Int> coordinateSet = new HashSet<Vector2Int>(coordinates);
-		GameManager.Instance.StartPos = coordinates;
-		// 查找起点：度为1的节点
-		Vector2Int start = FindCountIsOne(coordinates);
-		GameManager.Instance.StartPos.Remove(start);
-		LogManager.Instance.Log($"找到起点：X={start.x}, Y={start.y}");
-		// 初始化访问记录
-		HashSet<Vector2Int> visited = new HashSet<Vector2Int>();
-		// 初始化路径
-		List<InstructionData> path = new List<InstructionData>();
-
-		bool success = FindPath(start, coordinateSet, visited, path, null);
-		if (success)
-		{
-			//Debug.Log($"路径搜索成功，路径长度为：{path.Count}");
-			return path;
-		}
-		else
-		{
-			if (coordinates.Count == 4)
-			{//针对特殊情况
-				Vector2Int three = FindCountIsThree(coordinates);
-				if (three.x == -1 && three.y == -1)
-				{
-					LogManager.Instance.Log("1:未能找到覆盖所有节点的完整路径。");
-					LogManager.Instance.Log("___________________________深度搜索结束_____________________________");
-					return path;
-				}
-				else
-				{
-					//普通情况
-					for (int k = 0; k < coordinates.Count; k++)
-					{
-						if (three != coordinates[k])
-						{
-							InstructionData instruction = new InstructionData(coordinates[k], three);
-							path.Add(instruction);
-						}
-					}
-					return path;
-				}
-			}
-			else
-			{
-
-				LogManager.Instance.Log("1:未能找到覆盖所有节点的完整路径。");
-				LogManager.Instance.Log("___________________________深度搜索结束_____________________________");
-				return path;
-			}
-		}
-	}
-
+	
 
 
 	/// <summary>
@@ -385,84 +225,7 @@ public class UnitSDF : MonoBehaviour
 		visited.Remove(current);
 		return false;
 	}
-	#endregion
-
-	#region MyRegion
-
-	#endregion
-
-	/// <summary>
-	/// 记录当前节点及其未访问的邻居
-	/// </summary>
-	/// <param name="visitedNodes"></param>
-	/// <param name="start"></param>
-	/// <param name="index"></param>
-	/// <param name="around"></param>
-	private static void UpdateVisitedNodes(Dictionary<Vector2Int, InstDataCalculus> visitedNodes, Vector2Int start, int index, List<Vector2Int> around)
-	{
-		if (!visitedNodes.ContainsKey(start))
-		{
-			visitedNodes[start] = new InstDataCalculus(index, around);
-		}
-		else
-		{
-			visitedNodes[start].Index = index;
-		}
-		visitedNodes[start].SetFoldNodes(around[0]);
-	}
 
 
-	public static Vector2Int FindCountIsOne(List<Vector2Int> coordinates) {
-		HashSet<Vector2Int> coordinateSet = new HashSet<Vector2Int>(coordinates);
-		// 预计算所有坐标的邻居数量，以减少重复计算
-		Dictionary<Vector2Int, int> neighborsCountDict = new Dictionary<Vector2Int, int>();
-		foreach (Vector2Int pos in coordinates)
-		{
-			List<Vector2Int> neighbors = GameManager.Instance.GetAroundCanBeOperatedPos(pos.x, pos.y);
-			int count = neighbors.Count(n => coordinateSet.Contains(n));
-			neighborsCountDict[pos] = count;
-		}
-		Vector2Int start = new Vector2Int(-1,-1);
-		start = neighborsCountDict.Where(kvp => kvp.Value == 1)
-									  .Select(kvp => kvp.Key)
-									  .DefaultIfEmpty(new Vector2Int(-1, -1))
-									  .First();
-		if (start.x == -1&& start.y == -1)
-		{
-			// 没有找到则找邻居数量为3的
-			start = neighborsCountDict.Where(kvp => kvp.Value == 3)
-									  .Select(kvp => kvp.Key)
-									  .DefaultIfEmpty(new Vector2Int(-1, -1))
-									  .First();
-		}
-		if (start.x == -1 && start.y == -1)
-		{
-			// 如果依然没找到则找邻居数量大于1的
-			start = neighborsCountDict.Where(kvp => kvp.Value > 1)
-									  .Select(kvp => kvp.Key)
-									  .DefaultIfEmpty(new Vector2Int(-1, -1))
-									  .First();
-		}
-		return start;
-	}
 
-
-	public static Vector2Int FindCountIsThree(List<Vector2Int> coordinates)
-	{
-		HashSet<Vector2Int> coordinateSet = new HashSet<Vector2Int>(coordinates);
-		// 预计算所有坐标的邻居数量，以减少重复计算
-		Dictionary<Vector2Int, int> neighborsCountDict = new Dictionary<Vector2Int, int>();
-		foreach (var pos in coordinates)
-		{
-			var neighbors = GameManager.Instance.GetAroundCanBeOperatedPos(pos.x, pos.y);
-			int count = neighbors.Count(n => coordinateSet.Contains(n));
-			neighborsCountDict[pos] = count;
-		}
-		Vector2Int start = new Vector2Int(-1,-1);
-		start = neighborsCountDict.Where(kvp => kvp.Value == 3)
-									  .Select(kvp => kvp.Key)
-									  .DefaultIfEmpty(new Vector2Int(-1, -1))
-									  .First();
-		return start;
-	}
 }
